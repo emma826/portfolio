954203e07f55652800edc5e728631933a10b4882
Amoke Emmanuel <emmanuelamoke35@gmail.com>
2025-08-15 21:58:16 +0100

    version 0.9.2 sub 2

--- src/app/admin/interview-hub/page.jsx
+++ src/app/admin/interview-hub/page.jsx
@@ -14,6 +14,7 @@ import {
     DialogTitle,
     DialogTrigger,
 } from "@/components/ui/dialog"
+import { Textarea } from "@/components/ui/textarea"
 
 import { get_admin_interview_questions, submit_admin_questions } from "@/server_actions/interview-actions"
 
